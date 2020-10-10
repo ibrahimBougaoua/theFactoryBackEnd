161d3a130b25489cb56f100b2fa6dced22065d04
ibrahimBougaoua <ibrahim.bougaoua@etu.usthb.dz>
2020-10-10 09:09:10 -0700

    add SQLAlchemy

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask,redirect,session,request,jsonify,json,make_response
 from flask_bcrypt import Bcrypt
 from flask_cors import CORS
 
+from flask_sqlalchemy import SQLAlchemy
 import random
 import jwt
 from functools import wraps
@@ -10,8 +11,29 @@ from datetime import timedelta
 
 app = Flask(__name__)
 app.secret_key = '5df4hg5fg4jh56fg4j564gj564hg56j4g5h64j56hg4j5h45j45h4j'
+CORS(app)
 
+#SqlAlchemy Database Configuration With Mysql
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:''@localhost/usine'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+db = SQLAlchemy(app)
+
+app.config['BASE_URL'] = 'http://127.0.0.1:5003'  # Running on localhost
+app.config['JWT_SECRET_KEY'] = 'super-secret'  # Change this!
+app.config['JWT_TOKEN_LOCATION'] = ['cookies']
+app.config['JWT_COOKIE_CSRF_PROTECT'] = True
+app.config['JWT_CSRF_CHECK_FORM'] = True
+
+bcrypt = Bcrypt()
+
+#our model
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(50))
+
+    def __init__(self, name):
+        self.name = name
 # Route /login api Page
 @app.route('/login', methods=('GET','POST'))
 def login_jwt():
