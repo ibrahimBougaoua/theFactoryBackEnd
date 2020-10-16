4b2da1b331013ea62eb5a2ca65ed4d9f37a9c2fd
ibrahimBougaoua <ibrahim.bougaoua@etu.usthb.dz>
2020-10-16 01:48:49 -0700

    Refactoring : models/__init__

--- models/__init__.py
+++ models/__init__.py
@@ -5,6 +5,7 @@ app = Flask(__name__)
 
 app.secret_key = '5df4hg5fg4jh56fg4j564gj564hg56j4g5h64j56hg4j5h45j45h4j'
 
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:''@localhost/factory'
 
 db = SQLAlchemy(app)
