854e5f6f9a255c1c7dcc81adc9888cc5766eb899
ibrahimBougaoua <ibrahim.bougaoua@etu.usthb.dz>
2020-10-16 01:18:11 -0700

    Refactoring : factory model

--- models/factory.py
+++ models/factory.py
@@ -8,14 +8,14 @@ class Factory(db.Model):
     logo = db.Column(db.String(100))
     phone = db.Column(db.String(15))
     employee_id = db.Column(db.Integer, db.ForeignKey('employee.employee_id'),nullable=False)
-    payment_date = db.Column(db.String)
     created_at = db.Column(db.TIMESTAMP)
     updated_at = db.Column(db.TIMESTAMP)
 
-    def __init__(self, name, desc, logo, phone, created_at ,updated_at):
+    def __init__(self, name, desc, logo, phone, employee_id, created_at ,updated_at):
         self.name = name
         self.desc = desc
         self.logo = logo
         self.phone = phone
+        self.employee_id = employee_id
         self.created_at = created_at
         self.updated_at = updated_at
