9076995e7db7ed516b55b150069b0d87f0876d2d
ibrahimBougaoua <ibrahim.bougaoua@etu.usthb.dz>
2020-10-16 01:13:47 -0700

    Refactoring : factory model

---
+++ models/factory.py
@@ -0,0 +1,21 @@
+from . import db
+
+# factory Sales Model
+class Factory(db.Model):
+    factory_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    desc = db.Column(db.Text)
+    logo = db.Column(db.String(100))
+    phone = db.Column(db.String(15))
+    employee_id = db.Column(db.Integer, db.ForeignKey('employee.employee_id'),nullable=False)
+    payment_date = db.Column(db.String)
+    created_at = db.Column(db.TIMESTAMP)
+    updated_at = db.Column(db.TIMESTAMP)
+
+    def __init__(self, name, desc, logo, phone, created_at ,updated_at):
+        self.name = name
+        self.desc = desc
+        self.logo = logo
+        self.phone = phone
+        self.created_at = created_at
+        self.updated_at = updated_at
