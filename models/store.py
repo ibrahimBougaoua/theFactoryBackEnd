a3ded3b7e2d4867ddf9b8d9bff3868fefb6e1c80
ibrahimBougaoua <ibrahim.bougaoua@etu.usthb.dz>
2020-10-16 01:27:26 -0700

    Refactoring : store model

---
+++ models/store.py
@@ -0,0 +1,14 @@
+from . import db
+
+# store Model
+class Store(db.Model):
+    point_sale_id = db.Column(db.Integer, db.ForeignKey('point_of_sale.point_sale_id'),nullable=False)
+    product_id = db.Column(db.Integer, db.ForeignKey('product.product_id'),nullable=False)
+    quantity_store = db.Column(db.Integer)
+    quantity_sold = db.Column(db.Integer)
+
+    def __init__(self, point_sale_id, product_id, quantity_store, quantity_sold):
+        self.point_sale_id = point_sale_id
+        self.product_id = product_id
+        self.quantity_store = quantity_store
+        self.quantity_sold = quantity_sold
