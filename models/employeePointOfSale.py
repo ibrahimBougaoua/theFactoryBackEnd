01424f39126442331e46256adb1c6d36d2b7c4b4
ibrahimBougaoua <ibrahim.bougaoua@etu.usthb.dz>
2020-10-16 01:23:02 -0700

    Refactoring : employeePointOfSale model

---
+++ models/employeePointOfSale.py
@@ -0,0 +1,12 @@
+from . import db
+
+# employee point of sale Model
+class employeePointOfSale(db.Model):
+    employee_id = db.Column(db.Integer, db.ForeignKey('employee.employee_id'),nullable=False)
+    point_sale_id = db.Column(db.Integer, db.ForeignKey('point_of_sale.point_sale_id'),nullable=False)
+    date = db.Column(db.TIMESTAMP)
+
+    def __init__(self, employee_id, point_sale_id, date):
+        self.employee_id = employee_id
+        self.point_sale_id = point_sale_id
+        self.date = date
