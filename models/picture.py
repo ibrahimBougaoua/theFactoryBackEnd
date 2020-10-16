3b299ca4adc1325b308708a92e8176d0aebeb9c9
ibrahimBougaoua <ibrahim.bougaoua@etu.usthb.dz>
2020-10-16 01:17:23 -0700

    Refactoring : picture model

---
+++ models/picture.py
@@ -0,0 +1,15 @@
+from . import db
+
+# picture Sales Model
+class Picture(db.Model):
+    picture_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    size = db.Column(db.String(50))
+    product_id = db.Column(db.Integer, db.ForeignKey('product.product_id'),nullable=False)
+    created_at = db.Column(db.TIMESTAMP)
+
+    def __init__(self, name, size, product_id, created_at):
+        self.name = name
+        self.size = size
+        self.product_id = product_id
+        self.created_at = created_at
